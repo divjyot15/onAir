99b79f769b21def27bbbe31d115979bfbe95d790
Divjyot <divjyot00@gmail.com>
2021-08-09 13:43:07 +0530

    Changed folder of footer and advideo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { TrendingComponent } from './Mycomponents/trending/trending.component';
 import { ExclusiveComponent } from './Mycomponents/exclusive/exclusive.component';
 import { NowshowingComponent } from './Mycomponents/nowshowing/nowshowing.component';
 import { InternationalComponent } from './Mycomponents/international/international.component';
-import { FooterComponent } from './Mycomponent/footer/footer.component';
-import { AdvideoComponent } from './Mycomponent/advideo/advideo.component';
+import { FooterComponent } from './Mycomponents/footer/footer.component';
+import { AdvideoComponent } from './Mycomponents/advideo/advideo.component';
 
 @NgModule({
   declarations: [
