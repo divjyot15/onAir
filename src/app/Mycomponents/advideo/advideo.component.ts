eef38d6fc7b2f754fe2c1f03d5e6d03f665072d0
Divjyot <divjyot00@gmail.com>
2021-09-02 00:48:50 +0530

    Updated dist folder

--- src/app/Mycomponents/advideo/advideo.component.ts
+++ src/app/Mycomponents/advideo/advideo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { nowshowing } from 'src/app/shared/nowshowing';
 import { NowshowingService } from 'src/app/services/nowshowing.service';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Params,ActivatedRoute  } from '@angular/router';
 
 @Component({
@@ -12,13 +12,16 @@ import { Params,ActivatedRoute  } from '@angular/router';
 export class AdvideoComponent implements OnInit {
   video:nowshowing;
   now:nowshowing[];
+  genuineVideoUrl: SafeResourceUrl;
   constructor(private nowservice: NowshowingService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private sanitizer : DomSanitizer) { }
 
   ngOnInit(): void {
   this.now =this.nowservice.getnow();
   const title =this.route.snapshot.params['title'];
   this.video= this.nowservice.getbtitle(title);
+  this.genuineVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.video.url);
   }
 
 }
