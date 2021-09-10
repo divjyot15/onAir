70eb8808c84d2dfb74adc072dd0473b7d24c702e
Divjyot <divjyot00@gmail.com>
2021-09-11 02:30:43 +0530

    Allcollections page

--- src/app/Mycomponents/allcollections/allcollections.component.ts
+++ src/app/Mycomponents/allcollections/allcollections.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { trending } from 'src/app/shared/trending_collections';
+import { TrendingService } from 'src/app/services/trending.service';
+
 
 @Component({
   selector: 'app-allcollections',
@@ -7,9 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllcollectionsComponent implements OnInit {
 
-  constructor() { }
+  TC : trending[];
+  constructor(private trendingservices : TrendingService ) { }
 
   ngOnInit(): void {
+    this.TC = this.trendingservices.gettrending();
   }
-
 }
--- src/app/Mycomponents/collections/collections.component.ts
+++ src/app/Mycomponents/collections/collections.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TrendingService } from 'src/app/services/trending.service';
 import { Params,ActivatedRoute } from '@angular/router';
 import { trending } from 'src/app/shared/trending_collections';
+import { NowshowingService } from 'src/app/services/nowshowing.service';
+import { nowshowing } from 'src/app/shared/nowshowing';
 
 @Component({
   selector: 'app-collection',
@@ -10,13 +12,17 @@ import { trending } from 'src/app/shared/trending_collections';
 })
 export class CollectionsComponent implements OnInit {
   collection: trending;
-  
+  COLLECTIONS: trending[];
+  NS: nowshowing[];
+
   constructor(private route: ActivatedRoute,
-    private collectionservice: TrendingService) { }
+    private collectionservice: TrendingService,
+    private NOWSHOWING : NowshowingService) { }
 
   ngOnInit(): void {
     const title = this.route.snapshot.params['title'];
     this.collection = this.collectionservice.getbytitle(title);
-    
+    this.COLLECTIONS = this.collectionservice.gettrending();  
+    this.NS = this.NOWSHOWING.getnow();
   }
 }
